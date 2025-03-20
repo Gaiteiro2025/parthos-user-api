31d875d2e907e8a180e55abafab4be9db021d0cc
Gaiteiro2025 <allansgaiteiro+dev@gmail.com>
2025-03-20 14:34:31 -0300

    feat: template base de uma api com nest, docker e postgree

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
@@ -17,7 +14,6 @@ import { LocalStrategy } from './local.strategy';
       signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1h' },
     }),
   ],
-  controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [JwtStrategy],
 })
 export class AuthModule { }
